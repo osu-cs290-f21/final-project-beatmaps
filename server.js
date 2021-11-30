3a9f3c0abef8c6a93474b4484a1ce46c64aec054
Frankreed <baoanhtdn@gmail.com>
2021-11-29 21:48:16 -0800

    rebased from main

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
 const express = require('express')
-const events = require('./events')
 
 const app = express()
 
-events.searchConcertsOfArtist("frank ocean","australia").then(
-    (data)=>{
-        console.log(data)
-    },
-    (error)=>{
-        console.log('failed', error)
-    }
-)
-
 app.use(express.static('public'))
 
-app.get('*',)
-
-app.listen(3000)
+app.get('*', )
