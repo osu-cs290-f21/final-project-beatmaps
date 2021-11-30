13d7843cd8cd66cfb39c107cc0581539015f4419
Frankreed <baoanhtdn@gmail.com>
2021-11-30 14:24:24 -0800

    implemented a button to move the map to a different location, then added a marker for it. Also implemented a button to remove the marker.

--- public/index.js
+++ public/index.js
@@ -1,8 +1,38 @@
 let map;
+let markers = [];
 
 function initMap() {
+    const moveButton = document.getElementById("moveButton") //Button MUST be here for document to be recognized for some reason, maybe the google maps script is doing sth?
+    const deleteButton = document.getElementById("deleteButton")
+    let custom_pos = {
+        lat: 44.5632702,
+        lng: -123.2776147
+    }
+
     map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
+        center: {lat: -34.397, lng: 150.644},
         zoom: 8,
     });
+
+    moveButton.addEventListener("click", () => {
+        map.setCenter(custom_pos)
+        markers.push(new google.maps.Marker({
+            position: custom_pos,
+            map: map,
+        }))
+        console.log(markers)
+    })
+
+    deleteButton.addEventListener("click", () => {
+        setMapOnAll(null);
+        markers = []
+        console.log(markers)
+    })
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+    for (let i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
 }
