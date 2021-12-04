31bb3d9ee1343a6208658d1bfe5773d8152f72dd
Frankreed <baoanhtdn@gmail.com>
2021-12-03 17:10:56 -0800

    Spotify workssss yoooooooo

--- server.js
+++ server.js
@@ -1,38 +1,7 @@
-/**
- * Template for OAuth workflow taken from Spotify API docs. Modified to
- * be specific to our project
- */
-
-/**
- * This is an example of a basic node.js script that performs
- * the Authorization Code oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
- */
 const express = require('express'); // Express web server framework
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const spotify = require('./spotify');
-const events = require('./events');
-
-
-/**
- * Generates a random string containing numbers and letters
- * @param  {number} length The length of the string
- * @return {string} The generated string
- */
-
-// events.searchConcertsOfArtist("frank ocean","australia").then(
-//     (data)=>{
-//         console.log(data)
-//     },
-//     (error)=>{
-//         console.log('failed', error)
-//     }
-// )
-
 
 const app = express();
 
@@ -55,6 +24,10 @@ app.get('/topArtists', function () {
 app.get('/refresh_token', function (req, res) {
     spotify.refresh_token(req, res)
 });
+
+app.get('*', (req, res) => {
+    res.status(404).sendFile('/public/404.html')
+})
 
 console.log('Listening on 8888');
 app.listen(8888);
