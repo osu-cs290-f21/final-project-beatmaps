88acdc621ffcf87f72af3d4d2674ad19d473ae0b
Frankreed <baoanhtdn@gmail.com>
2021-12-05 22:32:00 -0800

    added spotify logins into server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const events = require('./events')
+const spotify = require('./spotify');
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express()
+
+app.use(express.static(__dirname + '/public'))
+    .use(cors())
+    .use(cookieParser());
 
 events.searchConcertsOfArtist(new Date(2021, 11, 5), new Date(2021, 11, 15), ["KALEO", "Jubilate!"], "Oregon").then(
     (data) => {
@@ -13,6 +20,19 @@ events.searchConcertsOfArtist(new Date(2021, 11, 5), new Date(2021, 11, 15), ["K
 )
 
 app.use(express.static('public'))
+
+//spotify login stuff
+app.get('/login', (req, res) => {
+    spotify.login(req, res)
+});
+
+app.get('/callback', function (req, res) {
+    spotify.auth(req, res)
+});
+
+app.get('/refresh_token', function (req, res) {
+    spotify.refresh_token(req, res)
+});
 
 app.get('*', (req, res) => {
     res.status(404).sendFile('/public/404.html')
