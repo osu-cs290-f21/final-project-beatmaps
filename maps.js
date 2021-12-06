bb8099eb662ad2c92dda64e33bd9966f734c132e
Frankreed <baoanhtdn@gmail.com>
2021-12-06 13:13:14 -0800

    geocode works consistently, replacing traveltime with calculating straight distance between 2 coordinates cuz i hit the traveltime quota

--- maps.js
+++ maps.js
@@ -5,48 +5,28 @@ process.env['TRAVELTIME_KEY'] = '606ddd0ff147c22f1280f7a981fc090a';
 
 const geocode_h = (query) => {
     const options = {
-        query: query,
+        query: query + " OR",
         within_country: 'USA',
-        limit: 1
+        limit: '1'
     }
     return traveltimejs.geocoding(options).then(
         (data) => {
-            console.log(query)
             return data.features[0].geometry.coordinates
         }).catch((e) => {
-        console.log(e)
+        throw e
     })
 }
 
-const distance_h = (origin, destinations) => {
-    const promise_list = []
-    let locations = [];
-    promise_list.push(
-        geocode_h(origin).then(data => {
-            locations = [{"id": "origin", "coords": {"lat": data[0], "lng": data[1]}}]
-        })
-    )
-    destinations.forEach(
-        (destin, index) => {
-            promise_list.push(
-                geocode_h(destin.split(", ").at(-1)).then(data => {
-                    locations.push({
-                        "id": `destination ${index}`,
-                        "coords": {"lat": data[0], "lng": data[1]}
-                    })
-                })
-            )
-        }
-    )
-    Promise.all(promise_list).then(
-        (data) => {
-            console.log("locations:", locations)
-            console.log("data:", data)
-        }
-    )
+const distance_h = (origin, destination) => {
+
 }
 
 module.exports = {
     distance: distance_h,
     geocode: geocode_h
 }
+
+
+//https://www.google.com/maps/dir/
+// Portland,+OR+97229/2300+SW+1st+Ave+%23103,+Portland,+OR+
+// 97201/@45.5319744,-122.793511,13z/data=!4m14!4m13!1m5!1m1!1s0x5495089368454bfb:0x5c0303c43f70b1f6!2m2!1d-122.807445!2d45.5593978!1m5!1m1!1s0x54950a14ccb00027:0x814751b8635553ce!2m2!1d-122.678275!2d45.506202!3e0
