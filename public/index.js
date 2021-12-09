c16a94ad76af0144cf71f2ff96d70b835c21bd88
Frankreed <baoanhtdn@gmail.com>
2021-12-09 01:26:43 -0800

    Maps js

--- public/index.js
+++ public/index.js
@@ -1,22 +1,78 @@
-function addCity(city) {
-    var context = {
-        city: city
-    }
-    var newCity = Handlebars.templates.city(context)
-    var results = document.getElementById('results-section')
-    results.insertAdjacentHTML('beforeend', newCity)
-}
+// Create the script tag, set the appropriate attributes
+const script = document.createElement('script');
+script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCleAO0L-ilQ0Vb-yGOmQHV6bfnfPg72TA&callback=initMap';
+script.async = true;
 
-function addEvent(artist, date, city) {
-    var context = {
-        artist: artist,
-        date: date,
-        city: city
+let map;
+let events;
+let circleRadius;
+
+// Attach your callback function to the `window` object
+window.initMap = function () {
+    let searchResultPromise = $.get("/searchGetResult", (data) => {
+        events = data
+        return data
+    })
+
+    let coordsPromise = $.get("/userCoords", (data) => {
+        return data
+    })
+
+    let slider = document.getElementById("radius-slider")
+    coordsPromise.then(
+        (data) => {
+            map = new google.maps.Map(document.getElementById("map"), {
+                center: {lat: data.coords[1], lng: data.coords[0]},
+                zoom: 6,
+            })
+
+            console.log(data)
+
+            circleRadius = new google.maps.Circle({
+                strokeColor: "#FF0000",
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: "#FF0000",
+                fillOpacity: 0.35,
+                map,
+                center: {lat: data.coords[1], lng: data.coords[0]},
+                radius: parseInt(data.radius) * 1609 //convert from miles to meters
+            })
+        }
+    )
+    slider.oninput = () => {
+        const radius = parseInt(slider.value)
+        circleRadius.setRadius(radius * 1609)
+        for (let i = 0; i < markers.length; i++) {
+            if (markers[i]["data"].distance <= radius) {
+                markers[i]["marker"].setMap(map)
+            } else {
+                markers[i]["marker"].setMap(null)
+            }
+        }
     }
-    var newEvent = Handlebars.templates.event(context)
-    var city = document.querySelectorAll('[city-name=' + city + ']')
-    city.insertAdjacentHTML('beforeend', newEvent)
-}
+
+    let markers = []
+    searchResultPromise.then(
+        (data) => {
+            console.log(data)
+            for (let i = 0; i < data.length; i++) {
+                markers.push({
+                        "data": data[i],
+                        "marker": new google.maps.Marker({
+                            position: {lat: data[i].coords[1], lng: data[i].coords[0]},
+                            map: map,
+                        })
+                    }
+                )
+            }
+        }
+    )
+};
+
+// Append the 'script' element to 'head'
+document.head.appendChild(script);
+
 
 //slider bar
 document.onreadystatechange = () => {
